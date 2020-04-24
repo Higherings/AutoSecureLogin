a0a181250d18d0b08c84215474ce7fc52f7928e8
Iván García <ivangarcias@gmail.com>
2020-04-24 16:03:36 -0600

    First realease

---
+++ autosecurelogin-cleaner.py
@@ -0,0 +1,72 @@
+import json
+import boto3
+import os
+import datetime
+from boto3.dynamodb.conditions import Key
+
+session = boto3.session.Session()
+dynamodb = session.resource('dynamodb')
+table = dynamodb.Table(os.environ['DDBTABLE'])
+ec2 = session.resource('ec2')
+nacl = ec2.NetworkAcl(os.environ['NACLID'])
+days_block = int(os.environ['BLOCKDAYS'])
+
+def lambda_handler(event, context):
+
+    free_rules = []
+    
+    # Finds date to clean
+    curr_date = datetime.datetime.now()
+    delta = datetime.timedelta(days=days_block)
+    clean_date = (curr_date - delta).strftime("%Y-%m-%d")
+
+    # Gets Rules by date to clean
+    rules = table.scan(
+        ProjectionExpression="pk, #rule",
+        Select="SPECIFIC_ATTRIBUTES",
+        FilterExpression=Key("date").lt(clean_date),
+        ExpressionAttributeNames={"#rule": "rule"}
+    )
+    for rule in rules['Items']:
+        rule_n = int(rule['rule'])
+        try:
+            response = nacl.delete_entry(Egress=False,RuleNumber=rule_n)
+            response = table.delete_item(Key={"pk":rule['pk']})
+            free_rules.append(rule_n)
+        except Exception as e:
+            print("Unable to remove Rule.")
+            print(e)
+    
+    while 'LastEvaluatedKey' in rules:
+        # Gets more Rules by date to clean
+        rules = table.scan(
+            ProjectionExpression="pk, #rule",
+            Select="SPECIFIC_ATTRIBUTES",
+            FilterExpression=Key("date").lt(clean_date),
+            ExpressionAttributeNames={"#rule": "rule"},
+            ExclusiveStartKey=rules['LastEvaluatedKey']
+        )
+        for rule in rules['Items']:
+            rule_n = int(rule['rule'])
+            try:
+                response = nacl.delete_entry(Egress=False,RuleNumber=rule_n)
+                response = table.delete_item(Key={"pk":rule['pk']})
+                free_rules.append(rule_n)
+            except Exception as e:
+                print("Unable to remove Rule.")
+                print(e)
+
+    print("Removed {} rules.".format(len(free_rules)))
+    
+    # Gets available Rule numbers
+    nextrule = table.get_item(Key={"pk":"nextrule"})
+    
+    # True if there are no rules yet
+    if not nextrule.get('Item'):
+        new_nextrule = set(free_rules)
+    else:
+        nextrule_list = list(nextrule['Item']['rule'])
+        new_nextrule = set(free_rules+nextrule_list)
+        response = table.put_item(Item={"pk":"nextrule","rule":new_nextrule,"lastdate":curr_date.strftime("%Y-%m-%d")})
+    
+    return None
---
+++ autosecurelogin.py
@@ -0,0 +1,94 @@
+import json
+import boto3
+import os
+
+session = boto3.session.Session()
+dynamodb = session.resource('dynamodb')
+table = dynamodb.Table(os.environ['DDBTABLE'])
+ec2 = session.resource('ec2')
+nacl = ec2.NetworkAcl(os.environ['NACLID'])
+
+SPACE = 5
+BASE_RULE = int(os.environ['BASERULE'])
+MAX_RULE = int(os.environ['MAXRULE'])*SPACE
+NACL_ID = os.environ['NACLID']
+
+def lambda_handler(event, context):
+    
+    # Gets DATA from event
+    e_date = event['detail']['service']['eventLastSeen']
+    e_ip = event['detail']['service']['action']['networkConnectionAction']['remoteIpDetails']['ipAddressV4']
+    e_country = event['detail']['service']['action']['networkConnectionAction']['remoteIpDetails']['country']['countryName']
+    e_type = event['detail']['type'].split('/')[1]
+    cidr = '.'.join(e_ip.split('.')[0:3])+'.0/24'
+    port = 0
+    
+    if e_type == 'RDPBruteForce': port = 3389
+    if e_type == 'SSHBruteForce': port = 22
+
+    # Gets Next Rule Number(s)
+    nextrule = table.get_item(Key={"pk":"nextrule"}) 
+
+    # TRUE if is not the first run
+    if nextrule.get('Item'):
+        nextrule_list = list(nextrule['Item']['rule'])
+        nextrule_list.sort()    # Rules numbers sorted from small to big
+        nextrule_n = int(nextrule_list.pop(0))
+        new_nextrule = set(nextrule_list)
+        
+        if not new_nextrule:
+            new_nextrule = {nextrule_n+SPACE}
+            
+    else:
+        nextrule_n = BASE_RULE # Regla inicial
+        new_nextrule = {nextrule_n+SPACE} 
+    
+    # Checks the limit of Rules to Create
+    if nextrule_n >= BASE_RULE + MAX_RULE:
+        nextrule_n = BASE_RULE + MAX_RULE
+        new_nextrule = {nextrule_n}
+        print("MAX_RULE reached.")
+        return None
+    
+    # Updates Rules in DynamoDB
+    try:
+        response = table.put_item(
+            Item = {
+                "pk":"cidr#"+cidr,
+                "country":e_country,
+                "rule":nextrule_n,
+                "date":e_date,
+                "type":e_type
+            },
+            ConditionExpression = "attribute_not_exists(pk)"
+        )
+    except Exception as e:
+        # If Rule already Exist, Updates new date
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            rule_db = table.get_item(Key={"pk":"cidr#"+cidr})
+            response = table.put_item(
+                Item = {
+                    "pk":rule_db['Item']['pk'],
+                    "country":rule_db['Item']['country'],
+                    "rule":int(rule_db['Item']['rule']),
+                    "date":e_date,
+                    "type":rule_db['Item']['type']
+                }
+            )
+            print("Rule updated: Rule {} BLOCK CIDR {} PORT {}".format(int(rule_db['Item']['rule']),cidr,port))
+    else:
+        # Updates Next Rule number in DynamoDB
+        response = table.put_item(Item={"pk":"nextrule","rule":new_nextrule,"lastdate":e_date})
+        
+        # Modifies NACL
+        response = nacl.create_entry(
+            CidrBlock=cidr,
+            Egress=False,
+            PortRange={'From':port, 'To':port},
+            Protocol="6", #TCP
+            RuleAction='deny',
+            RuleNumber=nextrule_n
+        )
+        print("Rule added: Rule {} BLOCK CIDR {} PORT {}".format(nextrule_n,cidr,port))
+    
+        return None
